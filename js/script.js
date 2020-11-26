d14e2328053cd04a38d385295501c14d42a1207a
Anita Korynek <gilkorynek@gmail.com>
2020-11-26 16:14:07 +0100

    Remove class active js/script.js

--- js/script.js
+++ js/script.js
@@ -8,6 +8,11 @@ function titleClickHandler(event){
   /* add class 'active' to the clicked link */
 
   /* remove class 'active' from all articles */
+  const activeLinks = document.querySelectorAll('.titles a.active');
+
+  for(let activeLink of activeLinks){
+  activeLink.classList.remove('active');
+}
 
   /* get 'href' attribute from the clicked link */
 
