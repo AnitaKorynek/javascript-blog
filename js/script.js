4edeb6a6d7b00a7936ddde9b00b7546cbef2bff3
Anita Korynek <gilkorynek@gmail.com>
2020-11-30 17:54:48 +0100

    Modified viev js/script.js

--- js/script.js
+++ js/script.js
@@ -8,36 +8,31 @@ function titleClickHandler(event){
   console.log('Link was clicked!');
   
   
-  
-
-  /* [DONE] remove class 'active' from all article links  */
+/* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
 }
 
-    
   /* [DONE] add class 'active' to the clicked link */
   clickedElement.classList.add('active');
   
 
   /* [DONE] remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll('.posts article.active');
+   const activeArticles = document.querySelectorAll('.posts article.active');
 
-  for(let activeArticle of activeArticles){
-    activeArticle.classList.remove('active');
+   for(let activeArticle of activeArticles){
+   activeArticle.classList.remove('active');
 }
 
-  /* [IN PROGRESS] get 'href' attribute from the clicked link */
+  /* [DONE] get 'href' attribute from the clicked link */
   const articleSelector = clickedElement.getAttribute('href');
   
-  
-
-  /* [IN PROGRESS]find the correct article using the selector (value of 'href' attribute) */
+  /* [DONE]find the correct article using the selector (value of 'href' attribute) */
   const targetArticle = document.querySelector(articleSelector);
   
-  /* add class 'active' to the correct article */
+  /* [DONE] add class 'active' to the correct article */
   targetArticle.classList.add('active');
 }
 
