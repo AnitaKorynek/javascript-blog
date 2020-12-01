4cd19517734f97923bdc57716fc6b5cf4d8e3e20
Anita Korynek <gilkorynek@gmail.com>
2020-12-01 12:28:15 +0100

    remove content of title list js/script.js

--- js/script.js
+++ js/script.js
@@ -49,6 +49,8 @@ const optArticleSelector = '.post',
 function generateTitleLinks(){
 
   /* remove contents of titleList */
+    const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = '';
 
   /* for each article */
 
