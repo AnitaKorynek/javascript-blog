0c3b19e5d91437c5f9d682821215a062f40408b2
Anita Korynek <gilkorynek@gmail.com>
2020-12-03 16:47:19 +0100

    split tags into array js/script.js

--- js/script.js
+++ js/script.js
@@ -83,32 +83,42 @@ function generateTitleLinks(){
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
+   
 }
-
 generateTitleLinks();
+
+
+
 
 function generateTags(){
   /* [DONE] find all articles */
   const articles = document.querySelectorAll(optArticleSelector);
+  console.log(articles);
 
   /* START LOOP: for every article: */
   for(let article of articles){
 
-  /* find tags wrapper */
+  /* [DONE]find tags wrapper */
   const tagsWrapper = article.querySelector(optArticleTagsSelector);
 
-  /* make html variable with empty string */
+  /* [DONE] make html variable with empty string */
+  let html = '';
 
-  /* get tags from data-tags attribute */
+  /* [DONE] get tags from data-tags attribute */
+  const articleTags = article.getAttribute('data-tags');
+  console.log(articleTags);
 
   /* split tags into array */
+  const articleTagsArray = articleTags.split(' ');
+  console.log(articleTagsArray);
+
 
   /* START LOOP: for each tag */
-
+  for(let tag of articleTagsArray){
   /* generate HTML of the link */
 
   /* add generated code to html variable */
-
+   }
   /* END LOOP: for each tag */
 
   /* insert HTML of all the links into the tags wrapper */
@@ -117,5 +127,6 @@ function generateTags(){
 
 }
 
+};
 
 generateTags();
